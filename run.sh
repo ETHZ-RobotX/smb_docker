d2f6b2d89ad9abeea955f03388e6173678f83fda
Frederic Debraine <fdebrain@student.ethz.ch>
2020-08-31 22:03:47 +0200

    feature: adding volume for code development in container

--- run.sh
+++ run.sh
@@ -3,4 +3,5 @@ xhost +
 docker run -ti --rm --name smb \
     -e DISPLAY=$DISPLAY \
     -v /tmp/.X11-unix:/tmp/.X11-unix  \
-    -v $(pwd)/bags:/home/bags smb
+    -v $(pwd)/bags:/home/bags \
+    -v $(pwd)/code:/home/code smb
