03ec9fba63b5ca50be2e39d9b6618382159ec181
Fu Zhengyu <zhengfuaj@gmail.com>
2024-04-18 15:45:12 +0000

    add docker cache & add helper command

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,9 +2,19 @@
 set -e
 
 ROOT=$(dirname "$(dirname "$(readlink -f $0)")")
+
+# Setup aliases
+echo "alias wssetup='source ${ROOT}/devel/setup.bash'" >> ~/.bashrc
+echo "alias wssetup='source ${ROOT}/devel/setup.zsh'" >> ~/.zshrc
+
+# make folder `src` if not exists
+if [ ! -d "${ROOT}/src" ]; then
+    mkdir -p "${ROOT}/src"
+fi
+
 # Clone the repository
 vcs import --input "${SMB_RAW_REPO_URL}/smb.repos" --recursive --skip-existing "${ROOT}/src"
 
 # setup catkin workspace
-catkin init --workspace "${ROOT}/src" 
-catkin config --workspace "${ROOT}/src" --extend /opt/ros/noetic -DCMAKE_BUILD_TYPE=Release
+catkin init --workspace "${ROOT}" 
+catkin config --workspace "${ROOT}" --extend /opt/ros/noetic -DCMAKE_BUILD_TYPE=Release
