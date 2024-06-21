11ccfdda62ddab73d8ecbedaeb4577428389262f
Fu Zhengyu <zhengfuaj@gmail.com>
2024-06-21 12:05:38 +0200

    separate x11 and vnc compose files

--- scripts/post-create.sh
+++ scripts/post-create.sh
@@ -1,20 +1,61 @@
 #!/bin/bash
 set -e
 
-print_instructions() {
-    echo "Keep it terminal running and run \`docker exec -it smb_container zsh\` in another terminal to enter the container."
+entrypoint() {
+    # Start the VNC server if X11 forwarding is not enabled
+    echo -e "\n\n"
+    if [ -z "$USE_X11_FORWARDING" ]; then 
+        echo "Info: USE_X11_FORWARDING is not set. Use VNC to access GUI applications."
+        echo "Info: Open \`localhost:5901\` in your VNC client to access the desktop."
+        echo "Info: or open \`localhost:6080\` in your web browser to access the desktop."
+        echo "Info: To use X11 forwarding, run \`docker compose -f compose-x11.yaml up\`."
+
+        echo "Starting VNC server..."
+        # change the resolution of the VNC server
+        sudo sed -i "s@\(export VNC_RESOLUTION=\)\".*\"@\1\"$VNC_RESOLUTION\"@" /usr/local/share/desktop-init.sh
+        # start the VNC server
+        bash /usr/local/share/desktop-init.sh
+
+        echo "VNC server started."
+    else
+    # Use x11 forwarding
+        echo "Info: USE_X11_FORWARDING is set. GUI applications will be displayed on the host."
+    fi
+
+    if [ -z "$USE_X11_FORWARDING" ]; then 
+        echo "Keep this terminal running and run \`docker exec -it smb_container_x11 zsh\` in another terminal to enter the container."
+    else
+        echo "Keep this terminal running and run \`docker exec -it smb_container bash\` in another terminal to enter the container."
+    fi
+
+    sleep infinity
 }
+
+USERNAME=$(whoami)
+
+echo "Post-create command started"
+echo "WORKSPACE_DIR: ${WORKSPACE_DIR}"
+echo "SMB_RAW_REPO_FILE_URL: ${SMB_RAW_REPO_FILE_URL}"
+echo "USE_X11_FORWARDING: ${USE_X11_FORWARDING}"
+echo "DISPLAY: ${DISPLAY}"
+echo "VNC_RESOLUTION: ${VNC_RESOLUTION}"
+echo "USER: ${USERNAME}"
 
 # Check if the container is initialized by checking if a checkpoint file exists
 if [ -f "/.initialized" ]; then
     echo "Workspace is already initialized. Skipping post-create command."
-    print_instructions
-    sleep infinity
+    entrypoint
 fi
 
 echo "Initializing workspace..."
 
-sudo chown -R ${USER}:${USER} ${WORKSPACE_DIR}
+# check if the workspace directory belongs to the user
+if [ -d "${WORKSPACE_DIR}" ]; then
+    if [ "$(stat -c %U ${WORKSPACE_DIR})" != "${USERNAME}" ]; then
+        echo "Changing ownership of workspace directory to ${USERNAME}"
+        sudo chown -R ${USERNAME}:${USERNAME} ${WORKSPACE_DIR}
+    fi
+fi
 
 ROOT="${WORKSPACE_DIR}"
 
@@ -42,7 +83,7 @@ fi
 vcs import --input "${SMB_RAW_REPO_FILE_URL}" --recursive --skip-existing "${ROOT}/src"
 
 # Setup catkin workspace
-catkin init --workspace "${ROOT}" 
+catkin init --workspace "${ROOT}" &> /dev/null
 
 # Configure the workspace
 catkin config --workspace "${ROOT}" \
@@ -51,8 +92,9 @@ catkin config --workspace "${ROOT}" \
 
 # save checkpoint
 sudo touch "/.initialized"
+
 echo -e "\n\n"
 echo "Workspace initialized successfully."
-print_instructions
-sleep infinity
+
+entrypoint
               
